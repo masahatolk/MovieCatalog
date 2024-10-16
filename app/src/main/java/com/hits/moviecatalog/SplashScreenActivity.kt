603449591b119826f6ac7e5d9d2995f08eaf074b
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2024-10-16 15:02:41 +0700

    Splash screen

--- app/src/main/java/com/hits/moviecatalog/SplashScreenActivity.kt
+++ app/src/main/java/com/hits/moviecatalog/SplashScreenActivity.kt
@@ -1,6 +1,22 @@
 package com.hits.moviecatalog
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1600)
+    }
 }
