3b51588d01123beb9ba51266009b8f2d9d39a68f
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2024-10-16 22:37:53 +0700

    Welcome screen

--- app/src/main/java/com/hits/moviecatalog/RegistrationActivity.kt
+++ app/src/main/java/com/hits/moviecatalog/RegistrationActivity.kt
@@ -8,5 +8,6 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
     }
 }
